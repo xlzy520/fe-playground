9660acdf8f0e4605320018907a7ea640667ee4b6
XLZY <1017866168@qq.com>
2018-03-06 14:28:27 +0800

    Vue+elementUI前端导入excel表格，预览，并分标签展示所有sheet

--- Vue-uploadExcel/VueStore.js
+++
@@ -1,8 +0,0 @@
-let lzy={}
-lzy.uploadExcelButton=
-	Vue.component('uploadExcelButton',{
-	template: '<div>' +
-	'        <el-button :loading="loading" type="primary" @click="handleUpload">选择excel上传</el-button>' +
-	'        <input id="excel-upload-input" type="file" accept=".xls .xlsx" class="c-hide" @change="handleFileChange">' +
-	'    </div>'
-})
