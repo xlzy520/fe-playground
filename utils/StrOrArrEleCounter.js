145bfa1c038c1fe068d6811cc356cfb3b1a19c93
zhibi <1017866168@qq.com>
2018-11-08 13:20:13 +0800

    增加返回String中每个元素出现的次数

---
+++ utils/StrOrArrEleCounter.js
@@ -0,0 +1,21 @@
+/**
+ * 统计数组中每个元素出现的次数，返回一个对象,使用Object.keys()获取去重后的数组，还需要将每个元素转为number型
+ * @param arr
+ * @returns Object
+ */
+export default function arrEleCounter(arr){
+  var b={}
+  for (let i = 0; i < arr.length; i++) {
+    b[arr[i]] = (b[arr[i]] + 1) || 1
+  }
+  return b
+}
+
+/**
+ * 返回String中重复出现的元素以及次数
+ * @param str   if('aabbcc11')
+ * @returns {*}  return 2a2b2c21
+ */
+export default function strEleCounter(str) {
+  return str.replace(/(\w)\1*/g,(reg,c)=>reg.length+c)  // \w匹配字母  \1匹配出现第一个一样的数组重复一次
+}
