e41ca178fb78bfe97572fbdcb00fe4df50b24cf7
zhibi <1017866168@qq.com>
2019-07-15 13:32:55 +0800

    文件上传nodejs示例

--- NodeJS/upload.js
+++ NodeJS/upload.js
@@ -1,52 +1,49 @@
-var express = require('express');
-var app = express();
-var fs = require("fs");
+var express = require('express')
+var app = express()
+var fs = require('fs')
 
-var bodyParser = require('body-parser');
-var multer  = require('multer');
+var bodyParser = require('body-parser')
+var multer = require('multer')
 
-app.use(express.static('public'));
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(multer({ dest: '/tmp/'}).array('image'));
+app.use(express.static('public'))
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(multer({ dest: '/tmp/' }).array('file'))
 
-app.get('/uploadTest.html', function (req, res) {
-  res.sendFile( __dirname + "/" + "phone.html" );
+app.get('/uploadTest.html', function(req, res) {
+  res.sendFile(__dirname + '/' + 'uploadTest.html')
 })
 
-app.get('/getInfo', function (req, res) {
+app.get('/getInfo', function(req, res) {
   res.send({
     code: 'ssss'
   })
 })
 
-app.post('/file_upload', function (req, res) {
-  
-  console.log(req.files[0]);  // 上传的文件信息
-  console.log(JSON.stringify(req.body));  // 附带的额外数据
-  
-  var des_file = __dirname + "/" + req.files[0].originalname;
-  fs.readFile( req.files[0].path, function (err, data) {
-    fs.writeFile(des_file, data, function (err) {
-      if( err ){
-        console.log( err );
-      }else{
+app.post('/file_upload', function(req, res) {
+  console.log(req.files[0]) // 上传的文件信息
+  console.log(JSON.stringify(req.body)) // 附带的额外数据
+
+  var des_file = __dirname + '/' + req.files[0].originalname
+  fs.readFile(req.files[0].path, function(err, data) {
+    fs.writeFile(des_file, data, function(err) {
+      if (err) {
+        console.log(err)
+      } else {
         response = {
-          message:'文件上传成功',
-          filename:req.files[0].originalname,
+          message: '文件上传成功',
+          filename: req.files[0].originalname,
           success: false
-        };
+        }
       }
-      console.log( response );
-      res.end( JSON.stringify( response ) );
-    });
-  });
+      console.log(response)
+      res.end(JSON.stringify(response))
+    })
+  })
 })
 
-var server = app.listen(8081, function () {
-  
+var server = app.listen(8081, function() {
   var host = server.address().address
   var port = server.address().port
-  
-  console.log("应用实例，访问地址为 http://%s:%s", host, port)
-  
+
+  console.log('应用实例，访问地址为 http://%s:%s', host, port)
 })
