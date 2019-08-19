e986b8c8582fb4c8de852c0b0468cd9bc58d3bf0
zhibi <1017866168@qq.com>
2019-08-19 16:47:41 +0800

    闭包用法，如将处理结果返回，当做缓存器，记录数据，如once只允许执行一次该方法

--- interview/闭包.js
+++ interview/闭包.js
@@ -1,12 +1,43 @@
-function fun(n,o) {
+function fun(n, o) {
   console.log(o)
   return {
-    fun:function(m){
-      return fun(m,n);
+    fun: function(m) {
+      return fun(m, n)
     }
-  };
+  }
 }
-var a = fun(0);  a.fun(1);  a.fun(2);  a.fun(3);//undefined,?,?,?
-var b = fun(0).fun(1).fun(2).fun(3);//undefined,?,?,?
-var c = fun(0).fun(1);  c.fun(2);  c.fun(3);//undefined,?,?,?
-//问:三行a,b,c的输出分别是什么？
+var a = fun(0); a.fun(1); a.fun(2); a.fun(3)// undefined,?,?,?
+var b = fun(0).fun(1).fun(2).fun(3)// undefined,?,?,?
+var c = fun(0).fun(1); c.fun(2); c.fun(3)// undefined,?,?,?
+// 问:三行a,b,c的输出分别是什么？
+
+// 闭包用法
+function getResources(fn) {
+  const cache = []
+  for (let i = 0; i < 6; i++) {
+    cache.push(i)
+  }
+  return fn(cache)
+}
+
+getResources(function(arr) {
+  console.log(arr)
+})
+
+// 只允许函数执行一次
+function once(fn) {
+  var called = false
+  return function() {
+    if (!called) {
+      called = true
+      // fn.apply(this, arguments)
+      fn.call(this, ...arguments)
+    }
+  }
+}
+
+const ss = once(function(sss) {
+  console.log(sss + 1)
+})
+ss(5)
+ss(4)
