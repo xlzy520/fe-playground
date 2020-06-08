e1c44e8c455a3441b1eb972f7408a5ef99e455e4
xlzy <1017866168@qq.com>
2020-06-08 11:59:52 +0800

    update: 测试strict mode

---
+++ examples/strict-mode.js
@@ -0,0 +1,11 @@
+var vendor_lib_dsadasdjasjdlasdjasldknasldna = function (e) {
+  var t= {}
+  t.sss = '312323'
+  var sss = function () {
+    "use strict";
+    function hhh() {
+      console.log(222);
+    }
+    hhh()
+  }
+}
