55abdf64df7e95570b93a2e4abd18a1c71718575
xlzy <1017866168@qq.com>
2021-04-15 18:06:28 +0800

    树与列表的互转

--- interview/JS/手写实现/列表转树.js
+++ interview/JS/手写实现/列表转树.js
@@ -17,7 +17,7 @@ let list = [
   {
     id: '2',
     title: '节点2',
-    parentId: ''
+    parentId: '1'
   },
   {
     id: '2-1',
@@ -25,3 +25,40 @@ let list = [
     parentId: '2'
   }
 ]
+
+
+function listToTree(list = []){
+  const mapping = list.reduce((pre, cur)=> {
+    pre[cur.id] = cur
+    cur.children = []
+    return pre
+  }, {})
+  const res = list.filter(v=> {
+    if (v.parentId) {
+      mapping[v.parentId].children.push(v)
+    }
+    return !v.parentId
+  })
+  console.log(mapping);
+  console.log(res);
+  return res
+}
+
+
+listToTree(list)
+
+console.log(treeToList(listToTree(list)));
+
+
+function treeToList (tree, result = [], level = 0) {
+  tree.forEach(node=>{
+    result.push(node)
+    node.level = level+1
+    if (node.children) {
+      treeToList(node.children, result, level+1)
+    }
+  })
+  return result
+}
+
+
